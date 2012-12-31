a369023bb621380ac51022cbe789dd180ba01562
charithjperera <charithjperera@localhost>
2012-12-31 20:03:17 +1100

    Added install for compilers as well as some scrits used with UART programming

---
+++ install_tools.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo 7zr x -o/usr/local/bin tools.7z
+sudo cp checksum /usr/local/bin/tools/extra/checksum
+sudo chmod a+wrx -R /usr/local/bin/tools
+#PATH=$PATH:/usr/local/bin/tools/bin:/usr/local/bin/tools/extra
+echo "Please add the following line to your .bashrc file:"
+echo "PATH=$""PATH:/usr/local/bin/tools/bin:/usr/local/bin/tools/extra"
+echo "If you are running 64 bit linux, please install the 32 bit compatibility libraries package: ia32-libs"
