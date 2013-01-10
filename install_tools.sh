21263f31624ad8d31bd66bf4599047f4fe7f0a8b
charith <charith@charith-laptop>
2013-01-10 22:46:44 +1100

    Small error in the compiler installation script, needed to make a folder for checksum.

--- install_tools.sh
+++ install_tools.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 sudo 7zr x -o/usr/local/bin tools.7z
+sudo mkdir /usr/local/bin/tools/extra/
 sudo cp checksum /usr/local/bin/tools/extra/checksum
 sudo chmod a+wrx -R /usr/local/bin/tools
 #PATH=$PATH:/usr/local/bin/tools/bin:/usr/local/bin/tools/extra
-echo "Please add the following line to your .bashrc file:"
+echo "Please add the following line to your .bashrc file without the quotes:"
 echo "PATH=$""PATH:/usr/local/bin/tools/bin:/usr/local/bin/tools/extra"
 echo "If you are running 64 bit linux, please install the 32 bit compatibility libraries package: ia32-libs"
